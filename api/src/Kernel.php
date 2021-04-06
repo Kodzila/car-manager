6579ce0c9625f5db661668bc8b288ea0c7350ca1
Pawel Kozik <pawel@linkfactory.dk>
2021-04-06 21:41:58 +0200

    Save

--- api/src/Kernel.php
+++ api/src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace Kodzila;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
