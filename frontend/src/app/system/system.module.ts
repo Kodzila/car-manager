a8d05799752a400990ee5d4952f38c1bd7801228
Pawel Kozik <pawel@linkfactory.dk>
2021-04-24 12:33:50 +0200

    Add toolbar

--- frontend/src/app/system/system.module.ts
+++ frontend/src/app/system/system.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import {ContainerPage} from "./container-page/container-page";
 import {FrontPage} from "./front-page/front-page";
 import {RouterModule} from "@angular/router";
+import {Toolbar} from './toolbar/toolbar';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 export const COMPONENTS = [
     ContainerPage,
     FrontPage,
+    Toolbar,
 ];
 
 export const EXPORTED_COMPONENTS = [];
@@ -17,6 +22,9 @@ export const EXPORTED_COMPONENTS = [];
     imports: [
         CommonModule,
         RouterModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
     ],
     providers: [],
 })
---
+++ frontend/src/app/system/toolbar/toolbar.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.html',
+  styleUrls: ['./toolbar.sass']
+})
+export class Toolbar {
+}
